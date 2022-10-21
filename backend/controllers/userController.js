fd3e8a2ccef790ed84f7d87767303c1df6cfaa66
Yu <zyken9054@gmail.com>
2022-10-21 15:40:02 -0400

    finished with backend auth set up, move to front end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,43 @@
 const User = require('../models/UserModel')
+const jwt = require('jsonwebtoken')
+
+// create token, _id is going to be part of the payload of the token
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '2d'})
+}
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const { email, password } = req.body
+    try {
+        // invoke the User model static method: login
+        const user = await User.login(email, password)
+
+        // if success login, create token here:
+        const token = createToken(user._id)
+
+        // let the server send back the email and send back the token so that the client(browser) knows who is authorized to view contents
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // sign up user
 const signupUser = async (req, res) => {
-    res.json({mssg: 'signup user'})
+    const { email, password } = req.body
+    try {
+        // invoke the User model static method: signup
+        const user = await User.signup(email, password)
+
+        // create token here:
+        const token = createToken(user._id)
+
+        // let the server send back the email and send back the token so that the client(browser) knows who is authorized to view contents
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -14,6 +16,66 @@ const userSchema = new Schema({
     }
 })
 
+// create own static method on user model, so that we can use those methods like some built in methods: .create(), .findById() etc..
+// pro tip: do not use arrow function if you are going to use "this"
+userSchema.statics.signup = async function(email, password) {
+    // check if the form is valid using validator
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
 
+    // check if email is valid
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    // check if the password is strong enough
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough, password needs to be at minimum of 8 characters, at least 1 lower & upper case character, at least 1 number and 1 symbol')
+    }
+
+    // check if email already exists in the database
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // hash the password
+    // step 1: generate salt
+    const salt = await bcrypt.genSalt(10)
+    // step 2: hash password
+    const hash = await bcrypt.hash(password, salt)
+
+    // store the hashed password along with the email in the db
+    const user = await this.create({ email, password: hash })
+
+    // don't forget to return the user!
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+    // check if either email and password has any values
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    // check if user already exists in the database
+    const user = await this.findOne({ email })
+
+    // if we cannot find the user with the email, then we dont bother to check for password match
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+
+    // compare the input password with the hashed version in the user document in db
+    const match = await bcrypt.compare(password, user.password)
+    
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
